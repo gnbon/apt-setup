f89aa14bdff2d29f69d71a6101e833730030444f
gnbon <borngunk7061@naver.com>
2020-09-09 17:08:01 +0900

    Edit: apt

--- apt/run.sh
+++ apt/run.sh
@@ -1,3 +1,5 @@
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo sed 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
-sudo apt update -y && sudo apt dist-upgrade -y
+sudo sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo sed -i 's/extras.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo apt update -y && sudo apt upgrade -y
