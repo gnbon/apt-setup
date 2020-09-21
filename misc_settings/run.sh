63ab5d8dddfe9169f05c8df4654f712434605009
gnbon <borngunk7061@naver.com>
2020-09-21 13:56:00 +0900

    feature:Edit misc settings 아이콘을 변경하였습니다. dconf editor를 이용해 dock을 변경하였습니다.

--- misc_settings/run.sh
+++ misc_settings/run.sh
@@ -7,6 +7,16 @@ imwheel -k
 # typing speed
 xset r rate 250
 
+# install icon theme numix-circle
+sudo apt install gnome-tweak-tool numix-icon-theme-circle
+
+# dconf-editor
+sudo apt install dconf-editor
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 38
+
 # Making korean folder name to English
 export LANG=C
 xdg-user-dirs-gtk-update
